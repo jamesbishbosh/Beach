ed6fbd030c6cb8be3a57bb30b647d7bb4784682f
James Bishop <james@bishbosh.ai>
2026-05-08 15:37:43 +0100

    feat: wrap next.config with Sentry

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,11 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "bishbosh-ventures-ltd",
+  project: "beach",
+  silent: !process.env.CI,
+});
+
